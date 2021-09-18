8bf77a04614965a029471e61a329b523f081abb9
Lune <lune9402@naver.com>
2021-09-18 16:04:03 +0900

    과제 풀이 진행중

--- app/src/main/java/com/example/listview_practice/MainActivity.kt
+++ app/src/main/java/com/example/listview_practice/MainActivity.kt
@@ -17,18 +17,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        mRoomList.add(RoomData(ContextCompat.getDrawable(this, R.drawable.room1)!!, "${8000}", "마포구 서교동, 1층",
+        mRoomList.add(RoomData(ContextCompat.getDrawable(this, R.drawable.room1)!!, "8000", "서교동, 1층",
         "망원/홍대역 풀옵션", "전세", "다세대건물", "오픈형 원룸"))
-        mRoomList.add(RoomData(ContextCompat.getDrawable(this, R.drawable.room2)!!, "${2}억 ${8},${500}", "마포구 서교동, 3층",
+        mRoomList.add(RoomData(ContextCompat.getDrawable(this, R.drawable.room2)!!, "2억 8,500", "서교동, 3층",
             "신혼부부의 보금자리", "전세", "다세대건물", "복층형 원룸"))
-        mRoomList.add(RoomData(ContextCompat.getDrawable(this, R.drawable.room3)!!, "${1}억 ${2000}", "마포구 서교동, 5층",
+        mRoomList.add(RoomData(ContextCompat.getDrawable(this, R.drawable.room3)!!, "1억 2,000", "서교동, 5층",
             "홍대입구역 인근 원룸 전세", "전세", "다세대건물", "오픈형 원룸"))
-        mRoomList.add(RoomData(ContextCompat.getDrawable(this, R.drawable.room4)!!, "${1}억 ${2000}", "마포구 성산동, 3층",
+        mRoomList.add(RoomData(ContextCompat.getDrawable(this, R.drawable.room4)!!, "1억 2,000", "성산동, 3층",
             "넓고 반듯한 오픈형 원룸", "전세", "다세대건물", "오픈형 원룸"))
-        mRoomList.add(RoomData(ContextCompat.getDrawable(this, R.drawable.room5)!!, "${1}억 ${2000}", "마포구 망원1동, 3층",
+        mRoomList.add(RoomData(ContextCompat.getDrawable(this, R.drawable.room5)!!, "1억 2,000", "망원1동, 3층",
             "풀옵션 원룸 전세", "전세", "다세대건물", "오픈형 원룸"))
-        mRoomList.add(RoomData(ContextCompat.getDrawable(this, R.drawable.room6)!!, "${1}억 ${5000}", "마포구 망원동, 2층",
+        mRoomList.add(RoomData(ContextCompat.getDrawable(this, R.drawable.room6)!!, "1억 5,000", "망원동, 2층",
             "초역세권★베란다 공간 있는 원룸", "전세", "다세대건물", "분리형 원룸"))
+
+//        String에 숫자 써지네.....
 
 
         val mAdapter = RoomAdapter(this, R.layout.room_list_item, mRoomList)
--- app/src/main/java/com/example/listview_practice/adapters/RoomAdapter.kt
+++ app/src/main/java/com/example/listview_practice/adapters/RoomAdapter.kt
@@ -17,21 +17,22 @@ class RoomAdapter(
     val mList : ArrayList<RoomData>
 ) : ArrayAdapter<RoomData>(mContext, resId, mList) {
 
-    val inf = LayoutInflater.from(mContext)
-
+    val inf = LayoutInflater.from(mContext) //이 화면을 기반으로
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
 
+//        convertView > 재사용성을 위한 장치
+
         var temprow = convertView
-
         if (temprow == null){
-
             temprow = inf.inflate(R.layout.room_list_item, null)
         }
 
-        val row = temprow!!
+        val row = temprow!! // !! <절대 null이 아니다!!
 
         val roomData = mList[position]
+
+//        row. 으로 들어가는 이유 ↑저기 실제 결과로 나갈  row 안에서 찾아봐라!
 
         val roomImg = row.findViewById<ImageView>(R.id.roomImage)
         val roomPrice = row.findViewById<TextView>(R.id.roomPriceTxt)
